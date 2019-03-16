38b29d62d33d4287ed7b976751c89bd07f940128
lollilol <lollilol@doorcraft.de>
2019-03-16 12:31:16 +0100

    add working_dir

--- install-fresh-system.sh
+++ install-fresh-system.sh
@@ -7,7 +7,7 @@ shell_theme="Arc-Dark"
 icon_theme="Papirus-Dark-Maia"
 aur_packages="spotify discord ttf-emojione ttf-google-sans ttf-opensans minecrafter-ttf jre winscp woeusb minecraft-launcher ttf-ms-fonts chromium-widevine"
 timezone="Europe/Berlin"
-
+working_dir=~/manjaro-fresh-install #the folder where the base script and the folder is in.
 
 read -p "Continue? Only do this, if you freshly installed manjaro! (y/N) " start
 
@@ -55,13 +55,13 @@ echo "2.1: Disable Sound Effects in Gnome"
 gsettings set org.gnome.desktop.sound event-sounds false
 echo "2.2: Backup Original Pulseaudio Config and copy the one from the home dir"
 sudo mv /etc/pulse/default.pa /etc/pulse/default.pa.orig
-sudo cp ~/bin/install-fresh-system-c/pulseaudio-default.pa /etc/pulse/default.pa
+sudo cp $working_dir/pulseaudio-default.pa /etc/pulse/default.pa
 echo "2.2.1: Restart Pulseaudio"
 pulseaudio -k
 sleep 2
 pulseaudio --start
 echo "2.3: Telegram Tray Autostart"
-cp ~/bin/install-fresh-system-c/telegramdesktop_tray.desktop ~/.config/autostart/telegramdesktop_tray.desktop
+cp $working_dir/telegramdesktop_tray.desktop ~/.config/autostart/telegramdesktop_tray.desktop
 echo "2.4: Various Time Configurations"
 echo
 sudo ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
@@ -70,13 +70,13 @@ echo
 echo "2.5 Init Razer Stuff"
 sudo gpasswd -a lollilol plugdev
 echo "2.5.1 Razer Blackwidow Init"
-sudo cp ~/bin/install-fresh-system-c/razer/init_blackwidow.py /root/init_blackwidow.py
-sudo cp ~/bin/install-fresh-system-c/razer/razer_blackwidow.rules /etc/udev/rules.d/razer_blackwidow.rules
+sudo cp $working_dir/razer/init_blackwidow.py /root/init_blackwidow.py
+sudo cp $working_dir/razer/razer_blackwidow.rules /etc/udev/rules.d/razer_blackwidow.rules
 sudo chmod a+x /root/init_blackwidow.py
 #echo "2.5.2 Razer Deathadder Elite Init"
 #echo
 #echo "2.6 Installing 31_hold_shift"
-#sudo cp ~/bin/install-fresh-system-c/31_hold_shift /etc/grub.d
+#sudo cp $working_dir/31_hold_shift /etc/grub.d
 #sudo chmod a+x /etc/grub.d/31_hold_shift
 #sudo update-grub
 
